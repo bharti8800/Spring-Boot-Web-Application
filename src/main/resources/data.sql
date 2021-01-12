a0f74e1541a9a8dcf4d8e6ca066f28e7f9f3e070
bharti8800 <bharti8800@gmail.com>
2021-01-12 01:56:56 -0500

    added data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+Insert into Item values(1, 'BlueTshirt', 2);
+Insert into Item values(2, 'Phone', 3);
+Insert into Item values(3, 'Chocolate', 2);
+Insert into Item values(4, 'Biscuit', 5);
+Insert into Item values(5, 'Pizza', 2);
+Insert into Item values(6, 'Pen', 7);
+Insert into Item values(7, 'Teddy', 1);
