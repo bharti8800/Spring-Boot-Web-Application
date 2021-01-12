58937807d16984b5fc394077c13f344d787b8afe
bharti8800 <bharti8800@gmail.com>
2021-01-12 02:10:47 -0500

    added create method for item

--- src/main/java/com/example/demo/Controller/ItemController.java
+++ src/main/java/com/example/demo/Controller/ItemController.java
@@ -1,13 +1,26 @@
 package com.example.demo.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.demo.ItemRepository;
+import com.example.demo.Model.Item;
 
 @Controller
 public class ItemController {
 
+	@Autowired
+	ItemRepository repo;
 	@RequestMapping("home")
 	public String home() {
+		return "home.jsp";
+	}
+	
+	@PostMapping("item")
+	public String createItem(Item item) {
+		repo.save(item);
 		return "home.jsp";
 	}
 }
