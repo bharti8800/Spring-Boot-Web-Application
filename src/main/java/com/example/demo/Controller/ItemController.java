082abe327c79932fd1f04c4f7b2e78f3d0e445f3
bharti8800 <bharti8800@gmail.com>
2021-01-14 19:25:25 -0500

    Added CRUD operations to the app

--- src/main/java/com/example/demo/Controller/ItemController.java
+++ src/main/java/com/example/demo/Controller/ItemController.java
@@ -1,9 +1,19 @@
 package com.example.demo.Controller;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.example.demo.ItemRepository;
 import com.example.demo.Model.Item;
@@ -13,15 +23,55 @@ public class ItemController {
 
 	@Autowired
 	ItemRepository repo;
-	@RequestMapping("home")
-	public String home() {
-		return "home.jsp";
+	
+	@RequestMapping("/home")
+	public ModelAndView home() {
+		ModelAndView mv = new ModelAndView();
+		List<Item> itemList = repo.findAll();
+		//System.out.println(itemList.toString());
+		mv.addObject("itemList",itemList.toString()); 
+		mv.setViewName("home.jsp");
+		return mv;
 	}
 	
 	
-	@PostMapping("item")
-	public String createItem(Item item) {
+	@PostMapping("/item")
+	public String createOrUpdateItem(Item item) {
 		repo.save(item);
 		return "home.jsp";
 	}
+	
+	
+	@GetMapping("/item")
+	public ModelAndView viewItem(@RequestParam("serialId") int serialId) {
+		ModelAndView mv = new ModelAndView();
+		Item item = repo.findById(serialId).orElse(null);
+		if(item!=null)
+			mv.addObject("item", item);
+		mv.setViewName("home.jsp");
+		return mv;
+	}
+	
+	
+	  @GetMapping("/deleteItem") 
+	  public String deleteItem(@RequestParam("id") int serialId) {
+		  Item item = repo.findById(serialId).orElse(null);
+		  if(item != null)
+			  repo.deleteById(serialId);
+		  return "home.jsp"; 
+	  }
+	 
+	
+	
+	  @RequestMapping("/items") 
+	  public ModelAndView viewItems() { 
+		  ModelAndView mv = new ModelAndView();
+		  List<Item> itemList = repo.findAll();
+		  mv.addObject("itemList",itemList.toString()); 
+		  mv.setViewName("home.jsp"); 
+		  return mv;
+	  
+	  }
+	 
+	
 }
--- src/main/webapp/home.jsp
+++ src/main/webapp/home.jsp
@@ -8,8 +8,15 @@
 </head>
 <body>
 <h2>Home page</h2>
+
+	Items
+	<ul>
+		<li> ${ itemList } </li>
+	</ul>
+	<br><br>
 <ul>
 	<li>Create a new Item</li>
+	<br><br>
 	<div name="div1">
 	Create a new Item
 	<form action="/item" method="post">
@@ -25,10 +32,38 @@
 	<input type="submit" value="Save">
 	</form>
 	</div>
+	<br><br><br>
 	<li>View all items</li>
+	<br><br>
 	<li>View Item</li>
+	<form method="GET" action="/item">
+	Serial Id: <input type="text" name="serialId"><br>
+	<input type="submit" value="submit"><br>
+	</form>
+	Serial id- ${item.serialId}, Item name-${item.name}
+	, Quantity- ${item.quantity}
+	<br><br><br>
 	<li>Update Item</li>
+	<form action="/item" method="post">
+	<label>Serial ID</label>
+	<input type="text" name="serialId">
+	<br>
+	<label>Name</label>
+	<input type="text" name="name">
+	<br>
+	<label>Quantity</label>
+	<input type="text" name="quantity">
+	<br>
+	<input type="submit" value="Update">
+	</form>
+	
+	<br><br><br>
 	<li>Delete Item</li>
+	<form action="/deleteItem" method="GET">
+	Serial Id -<input type="text" name="id">
+	<br>
+	<input type="submit" value="Delete">
+	</form>
 </ul>
 </body>
 </html>
